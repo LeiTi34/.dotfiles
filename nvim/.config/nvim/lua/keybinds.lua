f459da4749a3f6434834542030b664f504f43d4d
Alex Leidwein <alex.leidwein@bab.gv.at>
2021-10-05 17:53:30 +0200

    updated bufferline keybindings

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -35,11 +35,11 @@ map('n', '<Leader>l', ':FocusSplitRight<CR>', { silent = true })
 -- Bufferline
 -- These commands will navigate through buffers in order regardless of which mode you are using
 -- e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
-map('n', '<Leader>bt', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
-map('n', '<Leader>bT', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+map('n', 'gb', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+map('n', 'gB', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
 -- These commands will move the current buffer backwards or forwards in the bufferline
-map('n', '<Leader>bmt', ':BufferLineMoveNext<CR>', { noremap = true, silent = true })
-map('n', '<Leader>bmT', ':BufferLineMovePrev<CR>', { noremap = true, silent = true })
+map('n', 'g<C-b>', ':BufferLineMoveNext<CR>', { noremap = true, silent = true })
+map('n', 'g<C-B>', ':BufferLineMovePrev<CR>', { noremap = true, silent = true })
 -- These commands will sort buffers by directory, language, or a custom criteria
 map('n', '<Leader>be', ':BufferLineSortByExtension<CR>', { noremap = true, silent = true })
 map('n', '<Leader>bd', ':BufferLineSortByDirectory<CR>', { noremap = true, silent = true })
