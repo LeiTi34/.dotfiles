47190478cbce3043ca75fa42033afe7745acd41f
Alex Leidwein <a@b.c>
2026-04-01 10:46:16 +0200

    remove windsurf nvim plugin

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -382,17 +382,17 @@ return require('lazy').setup({
     --     'jcdickinson/http.nvim',
     --     build = 'cargo build --workspace --release'
     -- },
-    {
-        "Exafunction/windsurf.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
-        },
-        config = function()
-            require("codeium").setup({
-            })
-        end
-    },
+    -- {
+    --     "Exafunction/windsurf.nvim",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --         "hrsh7th/nvim-cmp",
+    --     },
+    --     config = function()
+    --         require("codeium").setup({
+    --         })
+    --     end
+    -- },
     {
         'hrsh7th/nvim-cmp',
         dependencies = {
