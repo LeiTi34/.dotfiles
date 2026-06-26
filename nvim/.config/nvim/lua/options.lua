3784052113f74dbefa654736de8f4acf5512ed91
Alex Leidwein <a@b.c>
2026-06-26 10:07:33 +0200

    feat: enable osc 52 in nvim

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -38,3 +38,10 @@ vim.o.wrap = true
 
 --[[
 vim.opt.completeopt = "menu,menuone,noselect" ]]
+
+local osc52 = require('vim.ui.clipboard.osc52')
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy  = { ['+'] = osc52.copy('+'),  ['*'] = osc52.copy('*')  },
+  paste = { ['+'] = osc52.paste('+'), ['*'] = osc52.paste('*') },
+}
